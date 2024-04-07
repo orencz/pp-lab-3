7017ba1fc974147c233c10ff0d58e54e53e1f3df
orencz <166212566+orencz@users.noreply.github.com>
2024-04-07 11:11:21 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        
         Point point = new Point(2.25, 3.25);
+        
         point.setX(4.25);
         point.setY(5.25);
-        System.out.println("Punkt ma współrzędne: x = " + point.getX() + ", y = " + point.getY());
-    }
-}
+        
+        System.out.println("Współrzędne: x = " + point.getX() + ", y = " + point.getY()); }}

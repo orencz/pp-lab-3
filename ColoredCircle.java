aace1b4d7fd788f732685532c240b8bdd88fd232
orencz <166212566+orencz@users.noreply.github.com>
2024-04-07 11:58:55 +0200

    ColoredCircle.java

---
+++ ColoredCircle.java
@@ -0,0 +1,9 @@
+public class ColoredCircle extends Circle {
+    private String color;
+
+    public ColoredCircle(Point center, double radius, String color) {
+        super(center, radius);
+        this.color = color;}
+
+    public String getColor() {
+        return color; }}

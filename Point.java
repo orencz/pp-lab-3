35b783d0e409ae3acc26deea82b36ffa6aa05424
orencz <166212566+orencz@users.noreply.github.com>
2024-04-07 11:12:41 +0200

    Point.java

--- Point.java
+++ Point.java
@@ -4,22 +4,16 @@ public class Point {
 
     public Point(double x, double y) {
         this.x = x;
-        this.y = y;
-    }
-
+        this.y = y;}
+    
     public double getX() {
-        return x;
-    }
-
+        return x;}
+    
     public double getY() {
-        return y;
-    }
-
+        return y;}
+    
     public void setX(double x) {
-        this.x = x;
-    }
-
+        this.x = x;}
+    
     public void setY(double y) {
-        this.y = y;
-    }
-}
+        this.y = y;}}

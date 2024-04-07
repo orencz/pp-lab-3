18000136c61d984defbe7f1b21981080454e410e
orencz <166212566+orencz@users.noreply.github.com>
2024-04-07 12:32:47 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,9 @@
-import geometry.Point;
-import geometry.Circle;
-import geometry.ColoredCircle;
-
 public class Main {
     public static void main(String[] args) {
-
-        Circle[] circles = new Circle[3];
         
-        circles[0] = new Circle(new Point(5, 5), 6); 
-        circles[1] = new ColoredCircle(new Point(6, 6), 7, "Zielony");
-        circles[2] = new Circle(new Point(7, 7), 8); 
+        Rectangle rectangle = new Rectangle(5.0, 10.0); 
 
-        
-        for (Circle circle : circles) {
-            System.out.println("Pole powierzchni kola: " + circle.getArea());}}}
+        System.out.println("Dlugosc: " + rectangle.getLength());
+        System.out.println("Szerokosc: " + rectangle.getWidth(
+        System.out.println("P: " + rectangle.calculateArea());
+        System.out.println("Obw: " + rectangle.calculatePerimeter());}}

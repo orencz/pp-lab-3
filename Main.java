e0d801243e90df4f899f184d645ba1332988a91d
orencz <166212566+orencz@users.noreply.github.com>
2024-04-07 12:36:20 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
         Rectangle rectangle = new Rectangle(5.0, 10.0); 
 
         System.out.println("Dlugosc: " + rectangle.getLength());
-        System.out.println("Szerokosc: " + rectangle.getWidth(
+        System.out.println("Szerokosc: " + rectangle.getWidth());
         System.out.println("P: " + rectangle.calculateArea());
         System.out.println("Obw: " + rectangle.calculatePerimeter());}}
